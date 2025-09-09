f17c5e97f0544d3948ab6d8e14f58342d4f2e8f7
Aluna0001 <lusz0001@stud.kea.dk>
2025-09-09 19:39:00 +0200

    user_login_functions

---
+++ lib/whoknows_elixir_monolith/accounts.ex
@@ -0,0 +1,20 @@
+defmodule WhoknowsElixirMonolith.Accounts do
+
+  import Ecto.Query, warn: false
+  alias WhoknowsElixirMonolith.Repo
+  alias WhoknowsElixirMonolith.Accounts.User
+end
+
+def authenticate_user(email, password) do
+  user = Repo.get_by(User, email: email)
+
+  if user && valid_password?(user , password) do
+    {:ok, user}
+  else
+    {:error, :invalid_credentials}
+  end
+end
+
+defp valid_password?(user, password) do
+  :crypto.hash(:sha256, password) == user.password_hash
+end
