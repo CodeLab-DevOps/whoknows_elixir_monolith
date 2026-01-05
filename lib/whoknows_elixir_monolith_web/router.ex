fed5166d766288a85432f3d9701032a4370a270a
AC25-C <151057176+AC25-C@users.noreply.github.com>
2026-01-05 02:01:07 +0100

    Last commit, everything works now

--- lib/whoknows_elixir_monolith_web/router.ex
+++ lib/whoknows_elixir_monolith_web/router.ex
@@ -16,6 +16,7 @@ defmodule WhoknowsElixirMonolithWeb.Router do
     plug :fetch_session
     # plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug WhoknowsElixirMonolithWeb.Plugs.RequestLogger
   end
 
   scope "/", WhoknowsElixirMonolithWeb do
